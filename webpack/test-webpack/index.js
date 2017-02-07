0e35608660fae5d993f121510ab564cda6e418f0
WYseven <consolelog@126.com>
2017-02-08 07:47:52 +0800

    增加webpack测试

--- webpack/test-webpack/index.js
+++ webpack/test-webpack/index.js
@@ -1,3 +1,24 @@
 /**
  * Created by wangyun on 17/2/5.
  */
+
+var foo = require("./src/foo");
+
+import styles from './src/css/tools.css';
+
+console.log(styles.abc);
+
+require("./src/sass/a.sass");
+
+require("./src/css/index.css");
+
+alert(1);
+
+//import foo form './src/foo'
+
+box.innerHTML = foo.fooTest+"<div class='"+styles.abc+"'>123</div>";
+
+
+
+
+
--- webpack/test-webpack/src/foo.js
+++ webpack/test-webpack/src/foo.js
@@ -1,3 +1,9 @@
 /**
  * Created by wangyun on 17/2/5.
  */
+var fooTest = 1000;
+
+exports.fooTest  = fooTest;
+
+
+
